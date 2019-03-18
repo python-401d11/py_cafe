b74f88261930bc29b5502f62802e179b526fbbba
TheMiloAnderson <themiloanderson@gmail.com>
2019-03-18 13:50:45 -0700

    first models tests

---
+++ src/tests/models_test.py
@@ -0,0 +1,14 @@
+from src.models import User, Customer, Employee, Order, OrdersContain, Item
+
+
+class TestUserModel():
+
+    def test_create_customer(self, customer):
+        assert customer
+
+    def test_customer_data(self, customer):
+        assert customer.name == 'Milo'
+        assert customer.email == 'milo@test.com'
+        assert customer.password == '12345'
+        assert customer.role == 'customer'
+        assert customer.phone == '123-456-7890'
