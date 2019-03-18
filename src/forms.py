87fd7669b63d8d80e1821a168971bf622d21bc92
Dan Le <Danhuyle508@gmail.com>
2019-03-18 11:45:14 -0700

    register and items form added

---
+++ src/forms.py
@@ -0,0 +1,17 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, SelectField, PasswordField
+from wtforms.validators import DataRequired
+from .models import User, Customer, Employee, Order, OrdersContain, Item
+from flask import g
+
+class RegisterForm(FlaskForm):
+    name = StringField('name', validators=[DataRequired()])
+    email = StringField('email', validators=[DataRequired()])
+    phone = StringField('phone', validators=[DataRequired()])
+    password = PasswordField('password', validators=[DataRequired()])
+
+class AddItemsForm(FlaskForm):
+    name = StringField('name', validators=[DataRequired()])
+    price = StringField('price',validators=[DataRequired()])
+    cost =  StringField('cost',validators=[DataRequired()])
+    count =  StringField('count',validators=[DataRequired()])
